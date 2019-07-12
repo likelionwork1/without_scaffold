adfbae7c561db54d765e9c385509d000a5b80789
Ubuntu <ubuntu@ip-172-31-34-75.us-east-2.compute.internal>
2019-07-12 17:54:09 +0000

    without_scaffold

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,43 @@
+class HomesController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.name = params[:post][:name]
+    @post.title = params[:post][:title]
+    @post.content = params[:post][:content]
+    @post.save
+    
+    redirect_to '/'
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.name = params[:post][:name]
+    @post.title = params[:post][:title]
+    @post.content = params[:post][:content]
+    @post.save
+    
+    redirect_to '/'
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    
+    redirect_to '/'
+  end
+end
